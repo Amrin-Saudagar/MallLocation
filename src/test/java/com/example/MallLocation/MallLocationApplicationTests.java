6073b95442dd9999b698bde1eaf084d92b4fcea5
Amrin-Saudagar <amrinbangi31@gmail.com>
2023-07-25 14:22:54 +0530

    This is a first project of Mall location service

---
+++ src/test/java/com/example/MallLocation/MallLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MallLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
